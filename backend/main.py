ac6c347d2b064393cdcc3aa71dc2cb4f7c0754d0
s2terminal <suzuki.sh@s2terminal.com>
2022-01-01 22:53:48 +0900

    fix api

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    return [
+        {
+            "title": "こんにちは",
+            "body": "今日はいい天気ですね"
+        },
+        {
+            "title": "こんばんは",
+            "body": "明日もいい天気だといいですね"
+        },
+    ]
